219e94048a76b2c7095479bcfda4aded8dbf5762
martin williams <86750566+martindw26@users.noreply.github.com>
2024-08-18 17:51:38 +0100

    UPDATED IN-CONTENT MPU BLOCK SCRIPT

--- MPU_ad_block.php
+++ MPU_ad_block.php
@@ -1,54 +1,57 @@
 <head>
-
-<style>
-.ad-container-in_content{
-    width: 400px;
-    height: 400px;
-    text-align: center;
-    display: block;
-    justify-content: center;
-    align-items: center;
-    background-color: #f2f2f2f2;
-    margin: 0 auto;
-    }
-
-    @media only screen and (max-width: 480px) {
-        .ad-container-in_content{
-            width:100%;
-            height: 350px;
-            background-color: #f2f2f242;
+    <style>
+        .ad-container-in_content {
+            max-width: 400px;
+            height: 400px;
+            text-align: center;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: #f2f2f2;
             margin: 0 auto;
+            padding: 10px;
+            box-sizing: border-box;
+        }
 
+        .ad-label p {
+            font-size: 14px;
+            color: #666;
+            margin: 0 0 10px;
+        }
+
+        @media only screen and (max-width: 768px) {
+            .ad-container-in_content {
+                max-width: 100%;
+                height: auto;
+                background-color: #f2f2f2;
             }
-        }  
+        }
 
+        @media only screen and (max-width: 480px) {
+            .ad-container-in_content {
+                height: 350px;
+                background-color: #f2f2f2;
+            }
+        }
+    </style>
 
-
-
-</style>
-<?php
-$in_content_mpu_bottom_script = get_field('in-content_mpu_script', 'option');
-?>
-
-
+    <?php
+    // Get the in-content MPU script field and sanitize output
+    $in_content_mpu_bottom_script = wp_kses_post(get_field('in-content_mpu_script', 'option'));
+    ?>
 </head>
 
 <!-- Ad block middle -->
-<div class="ad-container-in_content mt-4 mb-2">
+<aside class="ad-container-in_content mt-4 mb-2" role="complementary" aria-label="Advertisement">
     <div class="ad-label">
         <p>Advertisement</p>
     </div>
     <div class="mpu-in_content_block">
-      
-<?php 
-$in_content_mpus = get_field('in-content_mpus_body_script', 'option');
-echo $in_content_mpus;
-?>
-
-
-
-
-
+        <?php 
+        // Safely output the MPU script
+        $in_content_mpus = wp_kses_post(get_field('in-content_mpus_body_script', 'option'));
+        echo $in_content_mpus;
+        ?>
     </div>
-</div>
+</aside>
 <!-- End Ad block middle -->
