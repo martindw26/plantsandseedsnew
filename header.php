ff77e6cb6e821a6dfa1d14e950102691fbbb0afd
martin williams <86750566+martindw26@users.noreply.github.com>
2024-08-25 15:08:47 +0100

    echo $affiliate_scripts;

--- header.php
+++ header.php
@@ -28,6 +28,7 @@ $sidebar_mpu_middle_script = get_field('sidebar_mpu_middle_script', 'option');
 $sidebar_mpu_bottom_script = get_field('sidebar_mpu_bottom_script', 'option');
 //Affilaites
 $affiliate_scripts = get_field('affiliate_scripts', 'option');
+echo $affiliate_scripts;
 ?>
 
 </head>
